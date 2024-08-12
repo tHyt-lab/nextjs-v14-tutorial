06cbdfd53b01a1708a3fd4b2669fab671f44ad5c
tHyt-lab <washers.fezzes-0q@icloud.com>
2024-08-12 12:54:16 +0000

    chapter11 complete.

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,4 +1,4 @@
-import { Revenue } from "./definitions";
+import type { Revenue } from "./definitions";
 
 export const formatCurrency = (amount: number) => {
 	return (amount / 100).toLocaleString("en-US", {
@@ -7,10 +7,7 @@ export const formatCurrency = (amount: number) => {
 	});
 };
 
-export const formatDateToLocal = (
-	dateStr: string,
-	locale: string = "en-US",
-) => {
+export const formatDateToLocal = (dateStr: string, locale = "en-US") => {
 	const date = new Date(dateStr);
 	const options: Intl.DateTimeFormatOptions = {
 		day: "numeric",
